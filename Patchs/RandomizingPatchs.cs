47205196fc48199791712277af5d531e9194f3d8
EzeTxle <60270488+EzeTxle@users.noreply.github.com>
2023-09-16 13:51:37 -0300

    Update RandomizingPatchs.cs

--- Patchs/RandomizingPatchs.cs
+++ Patchs/RandomizingPatchs.cs
@@ -5,16 +5,8 @@ using System;
 using Game.Interface;
 using Server.Shared.State;
 
-namespace Patchs.Randomizing
+namespace Patchs
 {
-    [HarmonyPatch(typeof(PickNamesPanel), "Start")]
-    class InitGame
-    {
-        static public void Prefix()
-        {
-            if (ModSettings.GetBool("Randomize in-between games")) { OtherUtils.RandomizeSkinsNPet(); Console.WriteLine("Randomizing selections"); }
-        }
-    }
     [HarmonyPatch(typeof(Home.HomeScene.HomeSceneController), "Start")]
     class HomeScene
     {
